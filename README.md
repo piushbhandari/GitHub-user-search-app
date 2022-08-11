cca0f7c782707fdac47801e3cab9fb91364cd6bc
piush bhandari <piushthegreat@piushs-MacBook-Pro.local>
2022-08-11 12:24:08 -0500

    finished desktop

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # GitHub-user-search-app
+
+html / css / js
+
+https://git-hub-user-search-app-xi.vercel.app/
---
+++ script.js
@@ -0,0 +1,4 @@
+// get value from search input
+//fetch api
+//toggle light / dark mode -.-
+// set url based on search
